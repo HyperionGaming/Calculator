09b2fc2853b4e5c794d5c5dafb88a3b6b4a9a6ab
Maximus_yt <npkman333@gmail.com>
2024-03-16 22:01:41 -0400

    Initial commit

---
+++ src/main/java/net/hyperiongames/calculator/Calculator.java
@@ -0,0 +1,115 @@
+package net.hyperiongames.calculator;
+
+import net.hyperiongames.calculator.utils.Stopwatch;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.concurrent.TimeUnit;
+
+public class Calculator extends JFrame implements ActionListener {
+
+    private final JTextField textField;
+    private double firstOperand = 0;
+    private String operator = "";
+
+    public static void main(String[] args){
+        SwingUtilities.invokeLater(() ->{
+            Calculator calculatorApp = new Calculator();
+            calculatorApp.setVisible(true);
+        });
+    }
+
+    public Calculator(){
+        System.out.println("Starting Calculator...");
+
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.start();
+
+        setTitle("Calculator");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(300, 400);
+        setLayout(new BorderLayout());
+
+        ImageIcon icon = new ImageIcon("images/img.png");
+        Image image = icon.getImage();
+        setIconImage(image);
+
+        textField = new JTextField();
+        textField.setEditable(false);
+
+        Font font = textField.getFont();
+        textField.setFont(new Font(Font.DIALOG_INPUT, font.getStyle(), 50));
+
+        add(textField, BorderLayout.NORTH);
+
+        JPanel buttonPanel = new JPanel(new GridLayout(5, 4));
+
+        String[] buttonLabels = {
+                "", "", "^", "C",
+                "7", "8", "9", "÷",
+                "4", "5", "6", "×",
+                "1", "2", "3", "-",
+                "0", ".", "=", "+"
+        };
+
+        for(String label : buttonLabels){
+            JButton button = new JButton(label);
+            button.addActionListener(this);
+
+            buttonPanel.add(button);
+        }
+
+        add(buttonPanel, BorderLayout.CENTER);
+
+        stopwatch.stop();
+        System.out.println("Calculator has started in " + stopwatch.elapsedTime(TimeUnit.MILLISECONDS) + "ms.");
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event){
+        String command = event.getActionCommand();
+        if(command.equals("C")){
+            textField.setText("");
+
+        } else if(!(textField.getText().equalsIgnoreCase("NaN") || textField.getText().equalsIgnoreCase("Infinity"))){
+            if(command.matches("[0-9]")){
+                textField.setText(textField.getText() + command);
+
+            } else if(command.equals(".") && !textField.getText().isEmpty()){
+                if(!textField.getText().contains(".")){
+                    textField.setText(textField.getText() + ".");
+                }
+
+            } else if(command.matches("[×÷\\-+^]") && !textField.getText().isEmpty()){
+                firstOperand = Double.parseDouble(textField.getText());
+                operator = command;
+                textField.setText("");
+
+            } else if(command.equals("=") && !textField.getText().isEmpty()){
+                double secondOperand = Double.parseDouble(textField.getText());
+                double result = ((Double.isInfinite(performOperation(firstOperand, secondOperand, operator)) || (Double.isNaN(performOperation(firstOperand, secondOperand, operator))) ? Double.NaN : performOperation(firstOperand, secondOperand, operator)));
+                textField.setText(String.valueOf(result).replace(".0", ""));
+            }
+        }
+    }
+
+    private double performOperation(double operand1, double operand2, String operator){
+        return switch (operator){
+            case ("+") -> operand1 + operand2;
+            case ("-") -> operand1 - operand2;
+            case ("×") -> operand1 * operand2;
+            case ("^") -> Math.pow(operand1, operand2);
+            case ("÷") -> {
+                if(operand2 == 0){
+                    textField.setText("Cannot divide by zero");
+                    JOptionPane.showMessageDialog(this, "Cannot divide by zero");
+                    yield 0;
+                }
+                yield operand1 / operand2;
+            }
+            default -> 0;
+        };
+    }
+}
