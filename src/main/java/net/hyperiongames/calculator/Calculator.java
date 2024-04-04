e07b2ff93f7eaa073c50e34d6fcc584db91d1cf2
Maximus_yt <npkman333@gmail.com>
2024-04-04 18:01:19 -0400

    Added back the listener in the main class.

--- src/main/java/net/hyperiongames/calculator/Calculator.java
+++ src/main/java/net/hyperiongames/calculator/Calculator.java
@@ -1,19 +1,20 @@
 package net.hyperiongames.calculator;
 
-import lombok.Getter;
-import net.hyperiongames.calculator.listener.ButtonListener;
 import net.hyperiongames.calculator.utils.Stopwatch;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.concurrent.TimeUnit;
 
-@Getter
-public class Calculator extends JFrame {
+public class Calculator extends JFrame implements ActionListener {
 
-    @Getter private static Calculator instance;
+    public static Calculator instance;
 
-    private JTextField textField;
+    public JTextField textField;
+    private String operator = "";
+    private double firstOperand;
 
     public static void main(String[] args){
         SwingUtilities.invokeLater(() -> {
@@ -47,7 +48,7 @@ public class Calculator extends JFrame {
         setIconImage(image);
 
         textField = new JTextField();
-        textField.setEditable(false);
+        textField.setEditable(true);
 
         textField.setFont(new Font(Font.SANS_SERIF, textField.getFont().getStyle(), 50));
 
@@ -65,12 +66,57 @@ public class Calculator extends JFrame {
 
         for(String label : buttonLabels){
             JButton button = new JButton(label);
-            button.addActionListener(new ButtonListener());
+            button.addActionListener(this);
             button.setFont(new Font(Font.DIALOG_INPUT, button.getFont().getStyle(), 50));
 
             buttonPanel.add(button);
         }
 
         add(buttonPanel, BorderLayout.CENTER);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent event){
+        String command = event.getActionCommand();
+        if(command.equals("C")){
+            textField.setText("");
+
+        } else if(!(textField.getText().equalsIgnoreCase("NaN") || textField.getText().equalsIgnoreCase("Infinity"))){
+            if(command.matches("[0-9]")){
+                textField.setText(textField.getText() + command);
+
+            } else if(command.equals(".") && !textField.getText().isEmpty()){
+                if(!textField.getText().contains(".")){
+                    textField.setText(textField.getText() + ".");
+                }
+
+            } else if(command.matches("[×÷\\-+^]") && !textField.getText().isEmpty()){
+                firstOperand = Double.parseDouble(textField.getText());
+                operator = command;
+                textField.setText("");
+
+            } else if(command.equals("=") && !textField.getText().isEmpty()){
+                double secondOperand = Double.parseDouble(textField.getText());
+                double result = (Double.isInfinite(performOperation(firstOperand, secondOperand, operator)) || (Double.isNaN(performOperation(firstOperand, secondOperand, operator))) ? Double.NaN : performOperation(firstOperand, secondOperand, operator));
+                textField.setText(String.valueOf(result).replace(".0", ""));
+            }
+        }
+    }
+
+    private double performOperation(double operand1, double operand2, String operator){
+        return switch (operator){
+            case ("+") -> operand1 + operand2;
+            case ("-") -> operand1 - operand2;
+            case ("×") -> operand1 * operand2;
+            case ("^") -> Math.pow(operand1, operand2);
+            case ("÷") -> {
+                if(operand2 == 0){
+                    JOptionPane.showMessageDialog(Calculator.instance, "Cannot divide by zero");
+                    yield 0;
+                }
+                yield operand1 / operand2;
+            }
+            default -> 0;
+        };
     }
 }
